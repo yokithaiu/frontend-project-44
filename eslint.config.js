16203ac587690f2806a454fb601375df48e7d09b
yokithaiu <aloha.denser.me@mail.ru>
2025-04-13 15:00:27 +0300

    fix eslintconfig

--- eslint.config.js
+++ eslint.config.js
@@ -9,8 +9,8 @@ import pluginJs from '@eslint/js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat(
-  { baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended });
-
+  { baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended }
+);
 export default [
   { languageOptions: { globals: globals.browser } },
   ...compat.extends('airbnb'),
